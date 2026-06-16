c7a028972ec85ec869dba4103fb4800763f019e8
SerYuuu <sergiusggwp@gmail.com>
2026-06-17 04:09:45 +0700

    Add frontend assets to backend public

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -10,3 +10,6 @@ Route::get('/', function () {
         'status'  => 'running',
     ]);
 });
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
