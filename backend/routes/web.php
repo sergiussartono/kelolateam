617feabeb93f64e63cb7d8ececdde4bc40b54def
SerYuuu <sergiusggwp@gmail.com>
2026-06-17 07:52:51 +0700

    fix: serve frontend from public

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-    return response()->json([
-        'app'     => 'KelolaTeam API',
-        'version' => '1.0.0',
-        'status'  => 'running',
-    ]);
-});
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '.*');
